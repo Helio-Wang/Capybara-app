a6c19e53d6191ed5ff35e4b2e30d6bd8fb21a566
Helio <helio@Helios-iMac.local>
2019-10-11 07:53:16 -0700

    name change

--- main.py
+++ main.py
@@ -768,7 +768,7 @@ class ConvertWindow(MainAppWindow):
 class WelcomeWindow(qtw.QDialog):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('TRESSE')
+        self.setWindowTitle('Quokka')
         self.setMinimumWidth(500)
         self.setMinimumHeight(300)
         vlayout = qtw.QVBoxLayout()
